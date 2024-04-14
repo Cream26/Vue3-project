9b054365d077985ab5691d7ff0d824d39ce8ef9b
Cream26 <zhangtao4901@gmail.com>
2024-04-14 10:13:39 +0800

    delete sku complete

--- src/api/product/sku/index.ts
+++ src/api/product/sku/index.ts
@@ -1,17 +1,11 @@
 //SKU模块接口管理
 import request from '@/utils/request'
 import type { SkuResponseData, SkuInfoData } from './type'
-//枚举地址
 enum API {
-  //获取已有的商品的数据-SKU
   SKU_URL = '/admin/product/list/',
-  //上架
   SALE_URL = '/admin/product/onSale/',
-  //下架的接口
   CANCELSALE_URL = '/admin/product/cancelSale/',
-  //获取商品详情的接口
   SKUINFO_URL = '/admin/product/getSkuInfo/',
-  //删除已有的商品
   DELETESKU_URL = '/admin/product/deleteSku/',
 }
 //获取商品SKU的接口
--- src/api/product/sku/type.ts
+++ src/api/product/sku/type.ts
@@ -18,9 +18,9 @@ export interface SkuData {
   category3Id?: string | number //三级分类的ID
   spuId?: string | number //已有的SPU的ID
   tmId?: string | number //SPU品牌的ID
-  skuName?: string //sku名字
-  price?: string | number //sku价格
-  weight?: string | number //sku重量
+  skuName?: string
+  price?: string | number
+  weight?: string | number
   skuDesc?: string //sku的描述
   skuAttrValueList?: Attr[]
   skuSaleAttrValueList?: saleArr[]
